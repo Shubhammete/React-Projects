baac53b2801b02fce679c332c90fbca8b11c753c
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-11-15 12:28:38 +0530

    Added global state in App component

--- React Tic-Tac-Toe/TicTacToe/src/App.jsx
+++ React Tic-Tac-Toe/TicTacToe/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
-let SquareBox = ()=>{
-  // creating state which is independent of other blocks
-let [value,setValue]=useState("")
-let handleClick = ()=>{
-  setValue(value="X")
-}
+let SquareBox = ({value,handleClick})=>{
+
   return(
     <>
       <button className="box" onClick={handleClick}>{value}</button>
@@ -16,25 +12,30 @@ let handleClick = ()=>{
 
 
 function App() {
-
+  let [value,setValue]=useState(Array(9).fill(null))
+  let handleClick = (e)=>{
+    let arrcopy = value.slice()
+    arrcopy[e] = "X"
+    setValue(arrcopy)
+  }
   return (
     <>
     
       <h1>Tic-Tac-Toe</h1>
     <div className='rows'>
-    <SquareBox />
-    <SquareBox />
-    <SquareBox />
+    <SquareBox value={value[0]} handleClick={()=>{handleClick(0)}}/>
+    <SquareBox value={value[1]} handleClick={()=>{handleClick(1)}}/>
+    <SquareBox value={value[2]} handleClick={()=>{handleClick(2)}}/>
     </div>
     <div className='rows'>
-    <SquareBox />
-    <SquareBox />
-    <SquareBox />
+    <SquareBox value={value[3]} handleClick={()=>{handleClick(3)}}/>
+    <SquareBox value={value[4]} handleClick={()=>{handleClick(4)}}/>
+    <SquareBox value={value[5]} handleClick={()=>{handleClick(5)}}/>
     </div>
     <div className='rows'>
-    <SquareBox />
-    <SquareBox />
-    <SquareBox />
+    <SquareBox value={value[6]} handleClick={()=>{handleClick(6)}}/>
+    <SquareBox value={value[7]} handleClick={()=>{handleClick(7)}}/>
+    <SquareBox value={value[8]} handleClick={()=>{handleClick(8)}}/>
     </div>
     </>
   )
