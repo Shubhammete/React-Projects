2f078edfada367fad852cdf1272c55ea40499520
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-11-15 13:00:53 +0530

    Added winner checking function

--- React Tic-Tac-Toe/TicTacToe/src/App.jsx
+++ React Tic-Tac-Toe/TicTacToe/src/App.jsx
@@ -18,6 +18,11 @@ function App() {
   let [value,setValue]=useState(Array(9).fill(null))
   let handleClick = (e)=>{
     let arrcopy = value.slice()
+    // restrict updating box which has already updated
+    if(value[e]){
+      return
+    }
+    // switching player
     if(player){
     arrcopy[e] = "X"
     }else{
@@ -26,10 +31,19 @@ function App() {
     setValue(arrcopy)
     setPlayer(!player)
   }
+  // winner
+  let winner = calculateWinner(value);
+  let status = null;
+  if(winner){
+    status = "Winner is "+ winner
+  }else{
+    status = "Next Player is "+ (player ? "X" : "O")
+  }
   return (
     <>
     
       <h1>Tic-Tac-Toe</h1>
+      <h1>{status}</h1>
     <div className='rows'>
     <SquareBox value={value[0]} handleClick={()=>{handleClick(0)}}/>
     <SquareBox value={value[1]} handleClick={()=>{handleClick(1)}}/>
@@ -48,5 +62,27 @@ function App() {
     </>
   )
 }
+
+function calculateWinner(value) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (value[a] && value[a] === value[b] && value[a] === value[c]) {
+      return value[a];
+    }
+  }
+  return null;
+
+}
+
 
 export default App
