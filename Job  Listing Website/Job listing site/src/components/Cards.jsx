616d6d72a912eb2bbb22979c799c4f78b9898932
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-04-04 10:48:25 +0530

    Added Job listing using JSON file

---
+++ Job  Listing Website/Job listing site/src/components/Cards.jsx
@@ -0,0 +1,12 @@
+
+const Cards = ({children,bg=`bg-gray-100`}) => {
+  return (
+    <div>
+      <div className={`${bg} p-6 rounded-lg shadow-md`}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Cards
--- Job  Listing Website/Job listing site/src/components/HomeCards.jsx
+++ Job  Listing Website/Job listing site/src/components/HomeCards.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Cards from "./Cards"
 
 const HomeCards = () => {
   return (
@@ -7,7 +7,9 @@ const HomeCards = () => {
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+
+            {/** Wrapper class */}
+          <Cards>
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
@@ -18,8 +20,9 @@ const HomeCards = () => {
             >
               Browse Jobs
             </a>
-          </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+            </Cards>
+          {/* <div className="bg-indigo-100 p-6 rounded-lg shadow-md"> */}
+          <Cards bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
@@ -30,7 +33,7 @@ const HomeCards = () => {
             >
               Add Job
             </a>
-          </div>
+            </Cards>
         </div>
       </div>
     </section>
